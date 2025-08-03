9aa6292e04c1af9987f7d1e54360ac2ce6129b44
maivl <wkf39988@gmail.com>
2025-08-03 09:41:49 +0800

    Update start.sh

--- no-tls-sing-box-cloudflare/start.sh
+++ no-tls-sing-box-cloudflare/start.sh
@@ -2,6 +2,6 @@ docker run -d \
   --name singbox-cloudflare-no-tls \
   --restart always \
   -p 8002:8002 \
-  -v /home/vpnRoot/sing-box-cloudflare-no-tls/config.json:/etc/sing-box/config.json:ro \
+  -v /home/vpnRoot/no-tls-sing-box-cloudflare/config.json:/etc/sing-box/config.json:ro \
   ghcr.io/sagernet/sing-box:latest \
   run -c /etc/sing-box/config.json
